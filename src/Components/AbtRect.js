58152300589cfdd0f3d89b5a503005d3dfebc73b
Abubakarshykh <sheikhabubaker498@gmail.com>
2025-03-14 17:35:13 +0500

    addition of special cha

--- src/Components/AbtRect.js
+++ src/Components/AbtRect.js
@@ -18,15 +18,17 @@ export default function ImageWithText() {
                      bg-opacity-10 backdrop-blur-[5px] rounded-lg"
         >
           <h2 className="text-white text-lg font-semibold">
-            Feeling weighed down by<br /> thoughts that shouldn't be<br /> weighing you down?
-          </h2>
+  Feeling weighed down by<br /> thoughts that shouldn&apos;t be<br /> weighing you down?
+</h2>
+
           <button
             className="mt-4 text-md px-6 py-3 bg-[#222222] rounded-md text-white shadow-lg 
                        transform skew-x-[-20deg] relative overflow-hidden"
           >
             <div className="inline-block transform skew-x-[20deg]">
-              Reach Out, We're Ready!
-            </div>
+  Reach Out, We&apos;re Ready!
+</div>
+
           </button>
         </div>
       </div>
