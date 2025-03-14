2476b14a593539dda147a847ffa31b6d35482418
Abubakarshykh <sheikhabubaker498@gmail.com>
2025-03-14 16:45:37 +0500

    other changes for vercel deployment

--- src/Components/MeetTheLeaders.js
+++ src/Components/MeetTheLeaders.js
@@ -4,21 +4,21 @@ export default function MeetTheLeaders() {
   const leaders = [
     {
       name: "Yousuf Murad",
-      role: "Frontman (War Leader)",
+      role: "Polemarch",
       description:
         "As a frontman, I ensure seamless team excellence. My visionary strategies are molded in every project.",
       image: "/Aboutus/Yousuf.png",
     },
     {
-      name: "Yousuf Murad",
-      role: "Frontman (War Leader)",
+      name: "Rizwan Waseem",
+      role: "Strategos",
       description:
         "As Designers, lead teams to formulate solutions, weave new experiences, innovation, and reliability in every project.",
       image: "/Aboutus/Rizwan.png",
     },
     {
-      name: "Yousuf Murad",
-      role: "Frontman (War Leader)",
+      name: "Irtiza Sheikh",
+      role: "Hipparch",
       description:
         "As a frontman, I ensure seamless team excellence. My visionary strategies are molded in every project.",
       image: "/Aboutus/Irtiza.png",
@@ -28,7 +28,9 @@ export default function MeetTheLeaders() {
   return (
     <section className="bg-transparent text-white py-16 px-6">
       <div className="text-center mb-20">
-      <h1 className="text-[#CACACA] text-5xl font-bold my-6">MEET <span className="text-[#ffffff]">THE LEADERS</span></h1>
+        <h1 className="text-[#CACACA] text-5xl font-bold my-6">
+          MEET <span className="text-[#ffffff]">THE LEADERS</span>
+        </h1>
         <p className="text-gray-400 mt-2 max-w-2xl mx-auto">
           Our commitment is to elevate your digital journey, unlocking limitless
           possibilities in shaping a future where technology transforms visions
@@ -45,19 +47,29 @@ export default function MeetTheLeaders() {
               index === 1 ? "mt-[-100px]" : "mt-0"
             }`} // Middle card is elevated
           >
-            <div className="relative w-full h-80"> {/* Increased height for full image */}
+            <div className="relative w-full h-80">
+              {" "}
+              {/* Increased height for full image */}
               <Image
                 src={leader.image}
                 alt={leader.name}
-                width={280}  // Set fixed width
+                width={280} // Set fixed width
                 height={320} // Increased height to show full image
                 objectFit="contain" // Ensures full image is visible
                 className="rounded-lg"
               />
             </div>
-            <h3 className="font-bold text-lg text-white"> “{leader.name}”</h3>
-            <div className="text-[#FFAD00] text-center flex justify-center"><p className="text-sm max-w-[60%] rounded-xl font-bold">{leader.role}</p></div>
-            <p className="text-white mt-2 text-sm py-1 rounded-xl tracking-wider">{leader.description}</p>
+            <h3 className="font-bold text-lg text-white">
+              &ldquo;{leader.name}&rdquo;
+            </h3>
+            <div className="text-[#FFAD00] text-center flex justify-center">
+              <p className="text-sm max-w-[60%] rounded-xl font-bold">
+                {leader.role}
+              </p>
+            </div>
+            <p className="text-white mt-2 text-sm py-1 rounded-xl tracking-wider">
+              {leader.description}
+            </p>
           </div>
         ))}
       </div>
